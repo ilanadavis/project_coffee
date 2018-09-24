5d5d3d631b3c88c8ae7082af2eec8cd38f344094
Ilana Davis <ilana@ilanadavis.com>
2018-09-24 23:27:24 +0000

    add react and start setting up with new files

---
+++ src/components/ActionSelection.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class ActionSelection extends React.Component {
+ render() {
+   return <p>Heyyeye!</p>
+ } 
+}
+
+export default ActionSelection;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { render } from 'react-dom';
+import ActionSelection from './components/ActionSelection';
+
+render(<ActionSelection />, document.querySelector('#main'));
