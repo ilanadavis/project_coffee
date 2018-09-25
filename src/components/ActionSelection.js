71f4aa33863606b488273b6913b6c3e1f3305a07
Ilana Davis <ilana@ilanadavis.com>
2018-09-25 00:13:21 +0000

    add component for action selection. Add jsx from html for index page

--- src/components/ActionSelection.js
+++ src/components/ActionSelection.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 class ActionSelection extends React.Component {
  render() {
-   return <p>Heyyeye!</p>
+   return (
+    <React.Fragment> { /* can also import fragment and then remove react. in this line */ }
+      <button type="submit" className="paf-selection" href="paf_hire_index.html">New Hire</button><br />
+      <button type="submit" className="paf-selection" href="paf_change_index.html">Change</button><br />
+      <button type="submit" className="paf-selection" href="paf_termination_index.html">Term</button>
+    </React.Fragment>
+     )
  } 
 }
 
